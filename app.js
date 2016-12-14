a75207ac5e8f23dbfa3cf98502b9994ef5667143
amfang <JackyFang@JackydeMacBook-Air.local>
2016-12-14 22:53:41 +0800

    update app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+
+var exec = require('child_process').exec;
+var cmdStr = 'nohup ./bin/ssserver &';
+exec(cmdStr, function(err,stdout,stderr){
+  if(err) {
+    console.log('ssserver exec error:'+stderr);
+  }
+});
